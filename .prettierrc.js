a96052d31077c760953c1dcaa9388b8f9ece0664
roading <roading2022@outlook.com>
2022-05-23 14:40:30 +0800

    chore: add prettier eslint

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  editorconfig: true,  // .editorconfig file is in your project, Prettier will parse it and convert its properties to the corresponding Prettier configuration.
+
+  semi: false,
+  vueIndentScriptAndStyle: true,
+  trailingComma: 'all',
+  proseWrap: 'never',
+  htmlWhitespaceSensitivity: 'strict',
+  endOfLine: 'auto',
+};
